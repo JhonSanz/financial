b6bc07d3943f268cb442ffb5558bf24e05d49d71
JhonSanz <ingjhonsanz@utp.edu.co>
2022-02-18 18:05:42 -0500

    Trying to add indexes

--- apis/order/utils/excel_file.py
+++ apis/order/utils/excel_file.py
@@ -10,8 +10,22 @@ class FileReader:
     def convert(file):
         return pd.read_excel(file)
 
+    @staticmethod
+    def set_position_index(row):
+        i = 0
+        if row[1] == 'Fecha':
+            i += 1
+        return i
+
     def clean_file(self):
-        self.df = self.df[self.df['EPS'].notna()]
+        self.df.dropna(how='all', inplace=True)
+        self.df.dropna(axis=1, how='all', inplace=True)
+        self.df.reset_index(inplace=True)
+        self.df['position'] = 0
+        self.df['position'] = self.df[['index', self.df.columns[1]]].apply(
+            lambda x: self.set_position_index(x),
+            axis=1
+        )
 
     def validate_file(self):
         print(self.df)
